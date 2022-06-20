c9855173cca9ae3b0a3fdb16660c892d49943b7d
Simon <simon@weld.app>
2022-06-20 19:00:24 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div>Hello world...</div>
+      
+    </div>
+  )
+}
+
+export default App
